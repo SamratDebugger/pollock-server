979b9c3dcfa526869f7b78ac7484430f50663286
SamratDebugger <md721023@gmail.com>
2026-07-07 01:44:49 +0600

    cors setup done

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  }),
+);
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Hey World!");
+  res.send("Hey developers!");
 });
 
 app.listen(port, () => {
