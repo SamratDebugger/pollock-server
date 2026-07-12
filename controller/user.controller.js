e6a62c322fdb0897cf9a757e98d0fa4a4844ded3
SamratDebugger <md721023@gmail.com>
2026-07-13 03:14:45 +0600

    mongodb user connection

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,5 +4,24 @@ const userCollection = client.db("pollock").collection("users");
 
 export const createUser = async (req, res) => {
   const { username, email } = req.body;
-  console.log(username, email);
+
+  try {
+    const newUser = {
+      username,
+      email,
+      role: "member",
+      createdAt: new Date(),
+    };
+
+    await userCollection.insertOne(newUser);
+    res.status(201).json({
+      success: true,
+      user: newUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "User Can't Be Created",
+    });
+  }
 };
